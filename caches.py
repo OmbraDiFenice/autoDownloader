43db9d6a2c4bd343109dd9debc88468d0dac01b6
Stefano <stefano.stoduto@gmail.com>
2019-11-09 18:51:31 +0100

    add file cache

---
+++ caches.py
@@ -0,0 +1,35 @@
+import os
+
+
+class Cache:
+    pass
+
+
+class FileCache(Cache):
+    def __init__(self, path):
+        self._list = []
+        self.path = path
+        self._load()
+
+    def _load(self):
+        if not os.path.isfile(self.path):
+            open(self.path, "w").close()
+        else:
+            with open(self.path, "r") as f:
+                self._list = [elem.strip() for elem in f.readlines()]
+
+    def store(self, element):
+        self._list.append(element)
+
+    def clear(self):
+        self._list = []
+
+    def save(self):
+        with open(self.path, "w") as f:
+            f.writelines([line if line.endswith('\n') else line + '\n' for line in self._list])
+
+    def __len__(self):
+        return len(self._list)
+
+    def __iter__(self):
+        return self._list.__iter__()
---
+++ tests/testCache.py
@@ -0,0 +1,66 @@
+import unittest
+from caches import FileCache
+import os
+
+
+class TestFileCache(unittest.TestCase):
+    test_file_cache_path = "test_file_cache"
+    test_file_with_data_path = os.path.join("tests", "data", "file_cache_with_data")
+
+    def setUp(self):
+        self.delete_cache_file_is_exists()
+
+    def tearDown(self):
+        self.delete_cache_file_is_exists()
+
+    def delete_cache_file_is_exists(self):
+        if os.path.exists(self.test_file_cache_path):
+            os.remove(self.test_file_cache_path)
+
+    def test_create_file_cache(self):
+        FileCache(self.test_file_cache_path)
+        self.assertTrue(os.path.isfile(self.test_file_cache_path))
+
+    def test_file_cache_is_empty_when_created(self):
+        cache = FileCache(self.test_file_cache_path)
+        self.assertEqual(len(cache), 0)
+
+    def test_store_element(self):
+        cache = FileCache(self.test_file_cache_path)
+        cache.store("url1")
+        self.assertEqual(len(cache), 1)
+        self.assertTrue("url1" in cache)
+
+    def test_cache_contains_data_from_file_when_created(self):
+        cache = FileCache(self.test_file_with_data_path)
+        self.assertTrue("old url 1" in cache)
+        self.assertTrue("another old url" in cache)
+
+    def test_cache_is_emptied_when_clear_is_called(self):
+        cache = FileCache(self.test_file_with_data_path)
+        self.assertGreater(len(cache), 0)
+
+        cache.clear()
+        self.assertEqual(len(cache), 0)
+
+    def test_cache_is_persisted_on_file_when_save_is_called(self):
+        path = self.test_file_cache_path
+        new_elements = ["test url 1", "test url 2", "test url 3"]
+        cache = FileCache(path)
+
+        self.store_elements_in_cache(cache, new_elements)
+        cache.save()
+        self.assert_cache_file_contains_elements(new_elements, path)
+
+    def assert_cache_file_contains_elements(self, new_lines, path):
+        with open(path) as f:
+            file_lines = [line.strip() for line in f.readlines()]
+            self.assertEqual(new_lines, file_lines)
+
+    def store_elements_in_cache(self, cache, new_lines):
+        for line in new_lines:
+            cache.store(line)
+
+
+if __name__ == '__main__':
+    unittest.main()
