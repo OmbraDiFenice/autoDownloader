e0a6cb77114717da19b9836b95e166dc18a08179
Stefano <stefano.stoduto@gmail.com>
2019-11-09 19:05:19 +0100

    extract create_provider factory function

--- providers.py
+++ providers.py
@@ -10,15 +10,14 @@ class HtmlProvider(Provider):
     pass
 
 
-class ProviderFactory:
-    def create(self, specs):
-        if "type" not in specs.keys():
-            raise ProviderFactoryError("Unable to create provider: type not defined")
-        if specs.get("type") == "rss":
-            return RssProvider()
-        if specs.get("type") == "html":
-            return HtmlProvider()
-        raise ProviderFactoryError("Unable to create provider: type '{}' not known".format(specs.get("type")))
+def create_provider(specs):
+    if "type" not in specs.keys():
+        raise ProviderFactoryError("Unable to create provider: type not defined")
+    if specs.get("type") == "rss":
+        return RssProvider()
+    if specs.get("type") == "html":
+        return HtmlProvider()
+    raise ProviderFactoryError("Unable to create provider: type '{}' not known".format(specs.get("type")))
 
 
 class ProviderFactoryError(Exception):
--- tests/testProviderFactory.py
+++ tests/testProviderFactory.py
@@ -1,5 +1,5 @@
 import unittest
-from providers import ProviderFactory, RssProvider, HtmlProvider, ProviderFactoryError
+from providers import RssProvider, HtmlProvider, ProviderFactoryError, create_provider
 
 
 class TestProviderFactory(unittest.TestCase):
@@ -7,29 +7,23 @@ class TestProviderFactory(unittest.TestCase):
         provider_specs = {
             "name": "testProvider",
         }
-
-        provider_factory = ProviderFactory()
         self.assertRaisesRegex(ProviderFactoryError, r"Unable to create provider: type not defined",
-                               provider_factory.create, provider_specs)
+                               create_provider, provider_specs)
 
     def test_throw_error_if_specified_type_is_unknown(self):
         provider_specs = {
             "name": "testProvider",
             "type": "unknown"
         }
-
-        provider_factory = ProviderFactory()
         self.assertRaisesRegex(ProviderFactoryError, r"Unable to create provider: type 'unknown' not know",
-                               provider_factory.create, provider_specs)
+                               create_provider, provider_specs)
 
     def test_create_rss_provider(self):
         provider_specs = {
             "name": "test provider",
             "type": "rss"
         }
-
-        provider_factory = ProviderFactory()
-        provider = provider_factory.create(provider_specs)
+        provider = create_provider(provider_specs)
         self.assertIsInstance(provider, RssProvider)
 
     def test_create_html_provider(self):
@@ -37,9 +31,7 @@ class TestProviderFactory(unittest.TestCase):
             "name": "test provider",
             "type": "html"
         }
-
-        provider_factory = ProviderFactory()
-        provider = provider_factory.create(provider_specs)
+        provider = create_provider(provider_specs)
         self.assertIsInstance(provider, HtmlProvider)
 
 
